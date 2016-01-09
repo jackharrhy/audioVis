29a5639ae3583979d3e1541e6a72acfe51e3219c
Jack Arthur Harrhy <jack@jackharrhy.com>
2016-01-09 11:19:58 -0330

    init commmit

---
+++ js/initiation.js
@@ -0,0 +1,20 @@
+var audioContext  = new (window.AudioContext || window.webkitAudioContext)();
+var audio         = document.getElementById('audio');
+var audioSrc      = audioContext.createMediaElementSource(audio);
+var analyser      = audioContext.createAnalyser();
+var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+
+audioSrc.connect(analyser);
+analyser.connect(audioContext.destination);
+
+window.onload = function() {
+	document.getElementById('ready').innerHTML = 'Ready!';
+	document.getElementById('ui').style.backgroundColor = 'green';
+	document.getElementById('player').style.display = 'block';
+	start();
+}
+function start() {
+	document.getElementById('ui').style.display = 'none';
+	visualizers[0].initialFunction();
+	audio.play();
+}
